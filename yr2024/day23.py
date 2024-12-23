54d4fb3b9ac93333cee10c7f6565ab117cb8cdd4
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2024-12-22 23:38:29 -0600

    cleanup

--- yr2024/day23.py
+++ yr2024/day23.py
@@ -5,36 +5,21 @@ day = "23"
 lines = getlines(day)
 
 links = set([tuple(line.split("-")) for line in lines])
-graph = nx.Graph(links)
+
 
 links.update(((y, x) for x,y in list(links)))
 ts = set(x[0] for x in links if x[0][0] == 't')
-all_nodes = set(x[0] for x in links)
 
 triplets = set([])
 
 for x, y in links:
-    for t in all_nodes:
+    for t in ts:
         if (x, t) in links and (y, t) in links:
             triplets.add(frozenset([x, y, t]))
 
 print(len(triplets))
 
-def num_neighbors(node):
-    ret = 0
-    for n2 in all_nodes:
-        if (node, n2) in links:
-            ret += 1
-    return ret
-degrees = [(num_neighbors(node), node) for node in all_nodes]
-print(sorted(degrees))
-
-for triplet in triplets:
-    s = set(triplet)
-    for node in all_nodes:
-        if all((node, t) in links for t in s):
-            s.add(node)
-print(len(all_nodes))
+graph = nx.Graph(links)
 bylen = [(len(x), sorted(x)) for x in nx.find_cliques(graph)]
 bylen.sort()
 print(",".join(bylen[-1][1]))
